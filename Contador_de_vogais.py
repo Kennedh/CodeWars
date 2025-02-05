24fb246bd308c34d8074ad1e6fa6231d5b6f6914
kennedh <ken.cmarcellino@gmail.com>
2025-02-05 15:38:14 -0300

    Adição de comentário

--- Contador_de_vogais.py
+++ Contador_de_vogais.py
@@ -1,3 +1,11 @@
+"""
+Retorna o número (contagem) de vogais na string fornecida.
+
+Consideraremos a, e, i, o, u como vogais para este Kata (mas não y).
+
+A string de entrada consistirá apenas de letras minúsculas e/ou espaços.
+"""
+
 def get_count(w):
     vowels = "aeiou"
     return sum(1 for l in w if l in vowels)
