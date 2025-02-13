35003e97ce23b2fc39e31997601d6a7ade99df16
kennedh <ken.cmarcellino@gmail.com>
2025-02-13 12:48:40 -0300

    menor_número_lista

---
+++ menor_número_lista.py
@@ -0,0 +1,8 @@
+# Dado uma lista de inteiros, sua solução deve encontrar o menor inteiro.
+
+def find_smallest_int(arr):
+    return min(arr)
+
+lista = [2,8,6,-41]
+
+print(find_smallest_int(lista))
