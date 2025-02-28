030b1047915ba90a51a725c430192fa7d937b7ca
kennedh <ken.cmarcellino@gmail.com>
2025-02-28 20:11:23 -0300

    Create inteiro_para_string

---
+++ inteiro_para_string.py
@@ -0,0 +1,16 @@
+"""
+Precisamos de uma função que possa transformar um número (inteiro) em uma string.
+
+Quais maneiras de conseguir isso você conhece?
+"""
+
+def number_to_string(num):
+    return str(num)
+
+# Teste
+
+
+inteiro = 586
+
+print(type(inteiro))
+print(type(number_to_string(inteiro)))
