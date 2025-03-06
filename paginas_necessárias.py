c4589ac9bbea83cf8a647820247ce80217ad39d7
kennedh <ken.cmarcellino@gmail.com>
2025-03-06 20:59:05 -0300

    Create paginas_necessarias

---
+++ paginas_necessárias.py
@@ -0,0 +1,7 @@
+"""
+Seus colegas pediram para você copiar alguns papéis para eles. Você sabe que há 'n' colegas e os papéis têm 'm' páginas.
+
+Sua tarefa é calcular quantas páginas em branco você precisa. Se n < 0 ou m < 0, retorne 0.
+"""
+
+def paperwork(n, m):
