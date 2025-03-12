45c78f355154201e86c52faf4996f29795c62dee
kennedh <ken.cmarcellino@gmail.com>
2025-03-12 15:18:51 -0300

    Create alto_e_baixo

---
+++ alto_e_baixo.py
@@ -0,0 +1,10 @@
+"""
+Nesta pequena tarefa, você recebe uma sequência de números separados por espaços e deve retornar o maior e o menor número.
+"""
+
+def high_and_low(numbers):
+    return f"{max(map(int, numbers.split(' ')))} {min(map(int, numbers.split(' ')))}"
+
+# Teste
+
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
