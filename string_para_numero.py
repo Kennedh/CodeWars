527e1693f32889871be9af085b39abc5a000d33e
kennedh <ken.cmarcellino@gmail.com>
2025-03-15 17:47:28 -0300

    Create string_para_numero

---
+++ string_para_numero.py
@@ -0,0 +1,16 @@
+"""
+Precisamos de uma função que possa transformar uma string em um número. Quais maneiras de conseguir isso você conhece?
+
+Nota: Não se preocupe, todas as entradas serão strings,
+e cada string é uma representação perfeitamente válida de um número integral.
+"""
+
+def string_to_number(s):
+    return int(s)
+
+# Teste
+
+print(string_to_number("1234"))
+print(string_to_number("605"))
+print(string_to_number("1405"))
+print(string_to_number("-7"))
