2d3f460d821bb2df821ea56b7b62da61bd9144b8
kennedh <ken.cmarcellino@gmail.com>
2025-03-22 23:04:55 -0300

    .

---
+++ The Feast of Many Beasts.py
@@ -0,0 +1,13 @@
+"""
+Todos os animais estão tendo um banquete! Cada animal está trazendo um prato.
+Há apenas uma regra: o prato deve começar e terminar com as mesmas letras do nome do animal.
+Por exemplo, a garça-azul está trazendo naan de alho e o chapim está trazendo bolo de chocolate.
+
+Escreva uma função feast que receba o nome do animal e o prato como argumentos
+e retorne true ou false para indicar se o animal tem permissão para levar o prato para o banquete.
+
+Suponha que beast e dish sejam sempre strings minúsculas e que cada uma tenha pelo menos duas letras.
+beast e dish podem conter hifens e espaços, mas eles não aparecerão no início ou no final da string.
+Eles não conterão numerais.
+"""
+
--- string_para_numero.py
+++ string_para_numero.py
@@ -14,3 +14,4 @@ print(string_to_number("1234"))
 print(string_to_number("605"))
 print(string_to_number("1405"))
 print(string_to_number("-7"))
+
