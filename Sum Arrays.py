b2792564cfb4f625fe2775b9ffe4a0745265b381
kennedh <ken.cmarcellino@gmail.com>
2025-03-25 20:06:42 -0300

    Create Sum Arrays

---
+++ Sum Arrays.py
@@ -0,0 +1,11 @@
+"""
+Escreva uma função que pegue um array de números e retorne a soma dos números.
+Os números podem ser negativos ou não inteiros. Se o array não contiver nenhum número, você deve retornar 0.
+"""
+
+def sum_array(a):
+    return sum(a)
+
+# Teste
+
+print(sum_array([1, 5.2, 4, 0, -1]))
