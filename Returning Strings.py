bf86a3d56111972f63939f41d1cd594a919444cc
kennedh <ken.cmarcellino@gmail.com>
2025-04-13 21:52:13 -0300

    Create Returning Strings

---
+++ Returning Strings.py
@@ -0,0 +1,11 @@
+"""
+Crie uma função que aceite um parâmetro representando a namee retorne a mensagem:
+"Hello, <name> how are you doing today?".
+"""
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+# teste
+
+print(greet("Kennedh"))
