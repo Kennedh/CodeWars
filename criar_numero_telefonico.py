c52a7fe23916df9a22cff19af6fad3230ea13fee
kennedh <ken.cmarcellino@gmail.com>
2025-05-05 20:32:20 -0300

    Update v2 desafio

--- criar_numero_telefonico.py
+++ criar_numero_telefonico.py
@@ -1,7 +1,6 @@
 #Escreva uma função que aceite uma matriz de 10 números inteiros (entre 0 e 9)
 #e que retorne uma sequência desses números no formato de um número de telefone.
 
-#Eu gostaria de fazer nos moldes brasileiros mas para dar certo tem que ser validado no CodeWars
 #Então tem que retornar: (123) 456-7890
 
 def create_phone_number(n):
@@ -9,3 +8,8 @@ def create_phone_number(n):
 
 
 print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+
+# 05/05/2025 Treinando desafios feitos a uns certo tempo sem olhar como tinha feito antes
+
+def create_phone_number(n):
+    return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
