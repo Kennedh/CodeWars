7fb2121330519a014c8686ef23abd26d319b2663
kennedh <ken.cmarcellino@gmail.com>
2025-05-13 19:50:13 -0300

    Create Reverse List Order

---
+++ Reverse List Order.py
@@ -0,0 +1,12 @@
+"""
+Neste kata, você criará uma função que recebe uma lista e retorna uma lista com a ordem inversa.
+"""
+
+def reverse_list(l):
+    return l[::-1]
+
+# Teste
+
+print(reverse_list([1,8,2,7]))
+print(reverse_list([85,896,4,8579,1058]))
+
