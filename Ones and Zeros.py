2011ebdae9689f580424f8683a2b62cb7ecb9b5f
kennedh <ken.cmarcellino@gmail.com>
2025-05-20 20:57:55 -0300

    Create Ones and Zeros

---
+++ Ones and Zeros.py
@@ -0,0 +1,12 @@
+"""
+Dado um conjunto de uns e zeros, converta o valor binário equivalente em um inteiro.
+
+Ex: [0, 0, 0, 1]é tratado como 0001que é a representação binária de 1.
+"""
+
+def binary_array_to_number(arr):
+    return "".join(arr)
+
+# Teste
+
+print(binary_array_to_number([0, 0, 0, 1]))
