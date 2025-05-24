c3f941c501c31a51b6afb5a53ee06a92308f248a
kennedh <ken.cmarcellino@gmail.com>
2025-05-24 19:17:48 -0300

    Create Counting Duplicates

---
+++ Counting Duplicates.py
@@ -0,0 +1,22 @@
+"""
+Escreva uma função que retorne a contagem de caracteres alfabéticos e dígitos numéricos distintos,
+sem distinção entre maiúsculas e minúsculas, que ocorrem mais de uma vez na string de entrada.
+Pode-se presumir que a string de entrada contém apenas letras (maiúsculas e minúsculas) e dígitos numéricos.
+"""
+
+def duplicate_count(text):
+    t = ""
+    d = ""
+    c = 0
+    for l in text:
+        l = l.lower()
+        if l in t and l not in d:
+            c += 1
+            d += l
+        else:
+            t += l
+    return c
+
+# Teste
+
+print(duplicate_count("Indivisibilities"))
