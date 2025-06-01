73a2ec8a77f6fe7585f399c774c9ba05858edff5
kennedh <ken.cmarcellino@gmail.com>
2025-06-01 20:18:44 -0300

    Create Enumerable Magic #25 - Take the First N Elements

---
+++ Enumerable Magic #25 - Take the First N Elements.py
@@ -0,0 +1,10 @@
+"""
+Crie uma função que aceite uma lista/matriz e um número n e retorne uma
+lista/matriz dos primeiros n elementos da lista/matriz.
+"""
+
+def take(arr,n):
+    return arr[:n]
+
+# Teste
+
