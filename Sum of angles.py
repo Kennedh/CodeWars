7e8c2e8b6c0ee4475c93487ab58f444ee7593536
kennedh <ken.cmarcellino@gmail.com>
2025-06-09 19:33:07 -0300

    Create Sum of angles

---
+++ Sum of angles.py
@@ -0,0 +1,10 @@
+"""
+Encontre a soma total dos ângulos internos (em graus) em um polígono simples de n lados. N será maior que 2.
+"""
+
+def angle(n):
+    return (n - 2) * 180
+
+# Teste
+
+print(angle(2))
