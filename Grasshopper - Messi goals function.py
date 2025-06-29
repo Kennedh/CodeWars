6dfc61c37ffe1e6ed467d7b809efcc5ef679b5b1
kennedh <ken.cmarcellino@gmail.com>
2025-06-29 20:20:19 -0300

    Create Grasshopper - Messi goals function

---
+++ Grasshopper - Messi goals function.py
@@ -0,0 +1,17 @@
+"""
+Messi é um jogador de futebol com gols em três ligas:
+
+LaLiga
+Copa del Rey
+Campeões
+Complete a função para retornar seu número total de gols em todas as três ligas.
+
+Nota: a entrada será sempre válida.
+"""
+
+def goals(laLiga, copaDelRey, championsLeague):
+    return laLiga + copaDelRey + championsLeague
+
+# Teste
+
+print(goals(5, 10, 2))
