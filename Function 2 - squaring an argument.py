d603eb37d101c274c288f236fd5be2eb7723116d
kennedh <ken.cmarcellino@gmail.com>
2025-07-17 19:12:10 -0300

    Create Function 2 - squaring an argument

---
+++ Function 2 - squaring an argument.py
@@ -0,0 +1,12 @@
+"""
+Agora você precisa escrever uma função que receba um argumento e retorne o quadrado dele.
+"""
+
+def square(n):
+    return n * n
+
+# Teste
+
+print(square(2))
+print(square(5))
+print(square(67))
