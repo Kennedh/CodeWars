683578f30af26d919619da7c39863d8b509d1083
kennedh <ken.cmarcellino@gmail.com>
2025-07-18 17:51:40 -0300

    Create Simple Pig Latin

---
+++ Simple Pig Latin.py
@@ -0,0 +1,20 @@
+"""
+Mova a primeira letra de cada palavra para o final e adicione "ay" ao final da palavra. Não toque nos sinais de
+pontuação.
+"""
+
+def pig_it(text):
+    res = ""
+    for w in text.split():
+        if res == "":
+            res += w[1:] + w[0] + "ay"
+        else:
+            res += " " + w[1:] + w[0] + "ay"
+    return res
+
+# Teste
+
+
+
+print(pig_it('Pig latin is cool'))
+print(pig_it('This is my string'))
