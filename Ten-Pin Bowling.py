b4bb5c390f4916b4b88168eedfb5d081817ad3b4
kennedh <ken.cmarcellino@gmail.com>
2025-07-22 19:15:30 -0300

    Update Ten-Pin Bowling.py Finalizado

--- Ten-Pin Bowling.py
+++ Ten-Pin Bowling.py
@@ -23,17 +23,17 @@ def bowling_score(frames):
             elif p == "/":
                 rolls.append(10 - int(rolls[-1]))
             else:
-                rolls.append(p)
+                rolls.append(int(p))
     roll_idx = 0
     for n in range(10):
         if rolls[roll_idx] == 10:
-            total += int(rolls[roll_idx]) + int(rolls[roll_idx+1]) + int(rolls[roll_idx+2])
+            total += rolls[roll_idx] + rolls[roll_idx + 1] + rolls[roll_idx + 2]
             roll_idx += 1
-        elif int(rolls[roll_idx]) + int(rolls[roll_idx+1]) == 10:
-            total += int(rolls[roll_idx]) + int(rolls[roll_idx+1])
+        elif rolls[roll_idx] + rolls[roll_idx + 1] == 10:
+            total += rolls[roll_idx] + rolls[roll_idx + 1] + rolls[roll_idx + 2]
             roll_idx += 2
         else:
-            total += int(rolls[roll_idx]) + int(rolls[roll_idx+1])
+            total += rolls[roll_idx] + rolls[roll_idx + 1]
             roll_idx += 2
     return total
 
@@ -41,5 +41,4 @@ def bowling_score(frames):
 
 print(bowling_score('71 43 80 8/ 45 9/ 06 2/ 81 53')) # 97
 print(bowling_score('8/ 41 71 70 54 63 35 33 06 12')) # 75
-
-
+print(bowling_score("71 04 72 32 25 42 45 9/ 43 XX1")) # 90
