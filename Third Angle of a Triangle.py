8ec2831903d384dc20c66d6e22d81f4af4f440a8
kennedh <ken.cmarcellino@gmail.com>
2025-08-15 20:21:26 -0300

    Create Third Angle of a Triangle

---
+++ Third Angle of a Triangle.py
@@ -0,0 +1,10 @@
+"""
+São dados dois ângulos internos (em graus) de um triângulo.
+
+Escreva uma função para retornar o 3º.
+
+Observação: somente números inteiros positivos serão testados.
+"""
+
+def other_angle(a, b):
+    pass
