0587f14afe6c0267757fdb3d31dea63a7b524940
kennedh <ken.cmarcellino@gmail.com>
2025-08-23 17:12:55 -0300

    Create Human Readable Time

---
+++ Human Readable Time.py
@@ -0,0 +1,14 @@
+"""
+Escreva uma função que receba um inteiro não negativo (segundos) como entrada e retorne o tempo em um formato
+legível por humanos ( HH:MM:SS)
+
+HH= horas, preenchidas com 2 dígitos, intervalo: 00 - 99
+MM= minutos, preenchidos com 2 dígitos, intervalo: 00 - 59
+SS= segundos, preenchidos com 2 dígitos, intervalo: 00 - 59
+O tempo máximo nunca ultrapassa 359999 ( 99:59:59)
+
+Você pode encontrar alguns exemplos nos acessórios de teste.
+"""
+
+def make_readable(seconds):
+    pass
