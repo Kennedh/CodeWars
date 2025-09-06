bb40062a6c74059e024ed47850930b69ff9fd8f6
kennedh <ken.cmarcellino@gmail.com>
2025-09-05 23:53:06 -0300

    Create MakeUpperCase

---
+++ MakeUpperCase.py
@@ -0,0 +1,12 @@
+"""
+Escreva uma função que converta a string de entrada para letras maiúsculas.
+
+"""
+
+
+def make_upper_case(s):
+   return  s.upper()
+
+# Teste
+
+print(make_upper_case("teste"))
