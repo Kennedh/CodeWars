69e7393414c1992089316ab6fde905b6ba2489d4
kennedh <ken.cmarcellino@gmail.com>
2025-10-08 20:52:16 -0300

    update Count ones in a segment v2

--- Count ones in a segment.py
+++ Count ones in a segment.py
@@ -13,7 +13,7 @@ AVISO: O segmento pode conter bilhões de elementos. Para passar neste kata, sua
 
 # Primeira versão
 
-def count_ones(left, right):
+def count_onesv1(left, right):
     res = 0
     for n in range(left, right+1):
         res += str(bin(n)[2:]).count("1")
@@ -21,7 +21,7 @@ def count_ones(left, right):
 
 # Teste
 
-print(count_ones(1,10000))
+print(count_onesv1(1,13))
 
 """Como o codewars tem limite de timeout (12000ms), e para iterar bilhões de segmentos, leva muito tempo. Eu vou precisar de
  equação para tornar o código eficiente pois atualmente ele funciona mas para grandes volumes de números ele demora.
@@ -32,5 +32,21 @@ print(count_ones(1,10000))
 
 # x é número de casas minimas binarias, usando exemplo de 0-7 são no maximo 3
 
-x = 3
-print(x * 2 ** (x - 1))
+
+# Versão final
+
+def count_ones(left, right):
+    def soma_uns_ate(n):
+        if n <= 1:
+            return 1 if n == 1 else 0
+
+        x = n.bit_length() - 1
+        soma_bloco_principal = x * (1 << (x - 1))
+        soma_bits_restantes = n - (1 << x) + 1
+        recursao = soma_uns_ate(n - (1 << x))
+
+        return soma_bloco_principal + soma_bits_restantes + recursao
+
+    return soma_uns_ate(right) - soma_uns_ate(left - 1)
+
+print(count_ones(0,61401131303700))
