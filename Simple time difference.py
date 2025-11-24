6a9fbfd19627cbdf5bc2a181fa2f19f2ce8eac5a
kennedh <ken.cmarcellino@gmail.com>
2025-11-24 20:46:10 -0300

    Create Simple time difference.py

---
+++ Simple time difference.py
@@ -0,0 +1,43 @@
+"""
+In this Kata, you will be given a series of times at which an alarm sounds. Your task will be to determine the maximum
+time interval between alarms. Each alarm starts ringing at the beginning of the corresponding minute and rings for exactly one minute. The times in the array are not in chronological order. Ignore duplicate times, if any.
+
+For example:
+solve(["14:51"]) = "23:59". If the alarm sounds now, it will not sound for another 23 hours and 59 minutes.
+solve(["23:00","04:22","18:05","06:24"]) == "11:40". The max interval that the alarm will not sound is 11 hours and 40
+minutes.
+In the second example, the alarm sounds 4 times in a day.
+
+More examples in test cases. Good luck!
+"""
+
+
+def solve(times):
+    minutes = []
+    for time in times:
+        h, m = map(int, time.split(':'))
+        minutes.append(h * 60 + m)
+
+    minutes = sorted(set(minutes))
+
+    if len(minutes) == 1:
+        return "23:59"
+
+    minutes.append(minutes[0] + 24 * 60)
+
+    max_interval = 0
+    for i in range(1, len(minutes)):
+        diff = minutes[i] - minutes[i - 1] - 1
+        if diff > max_interval:
+            max_interval = diff
+
+    hours = max_interval // 60
+    mins = max_interval % 60
+
+    return f"{hours:02d}:{mins:02d}"
+
+
+# Teste
+
+print(solve(["14:51"]))
+print(solve(["23:00", "04:22", "18:05", "06:24"]))
