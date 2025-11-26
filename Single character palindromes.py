1e633ffaff4d30a9eb86212f584be61f22511f8b
kennedh <ken.cmarcellino@gmail.com>
2025-11-26 19:41:15 -0300

    Create Single character palindromes

---
+++ Single character palindromes.py
@@ -0,0 +1,35 @@
+"""
+You will be given a string and you task is to check if it is possible to convert that string into a palindrome by
+removing a single character. If the string is already a palindrome, return "OK". If it is not, and we can convert it to
+a palindrome by removing one character, then return "remove one", otherwise return "not possible". The order of the
+characters should not be changed.
+
+For example:
+
+solve("abba") = "OK". -- This is a palindrome
+solve("abbaa") = "remove one". -- remove the 'a' at the extreme right.
+solve("abbaab") = "not possible".
+More examples in the test cases.
+
+Good luck!
+"""
+
+def solve(s):
+    temp = []
+    if s == s[::-1]:
+        return "OK"
+    for idx, l in enumerate(s):
+        temp = list(s)
+        del temp[idx]
+        if "".join(temp[::-1]) == "".join(temp):
+            return "remove one"
+    return "not possible"
+
+# Teste
+
+print(solve("abba"))
+print(solve("abbaa"))
+print(solve("abbaab"))
+print(solve("madmam"))
+print(solve("raydarm"))
+print(solve("hannah"))
