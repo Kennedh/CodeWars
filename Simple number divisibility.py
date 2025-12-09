bb2e0374f4b4d3798bcb19adbea19788c3718b28
kennedh <ken.cmarcellino@gmail.com>
2025-12-08 23:58:12 -0300

    Create Simple number divisibility.py

---
+++ Simple number divisibility.py
@@ -0,0 +1,27 @@
+"""
+Neste Kata, você receberá um número e sua tarefa será reorganizá-lo para que seja divisível por 25, mas sem zeros à
+esquerda. Retorne o número mínimo de movimentos de dígitos necessários para tornar isso possível. Se impossível,
+retorne -1( Nothingem Haskell ).
+
+Por exemplo:
+
+solve(521) = 3 because:
+    a) Move the digit '1' to the front: 521 -> 512 -> 152. The digit '1' is moved two times.
+    b) Move '5' to the end: 152 -> 125. The digit '5' is moved one time, so total movement = 3.
+Of all the ways to accomplish this, the least digit moves = 3.
+
+solve(100) = 0. Number already divisible by 25.
+solve(1) = -1. Not possible to make number divisible by 25.
+
+solve(0) is not tested.
+Mais exemplos em casos de teste.
+
+Boa sorte!
+"""
+
+def solve(n):
+    pass
+
+# Teste
+
+print("128625452".find("a"))
