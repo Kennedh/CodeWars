c814f6fbd85fc2f5b8182321fe3f5989789aad02
kennedh <ken.cmarcellino@gmail.com>
2026-03-02 20:51:18 -0300

    Update Em construção AGUARDE

--- Next bigger number with the same digits.py
+++ Next bigger number with the same digits.py
@@ -13,4 +13,18 @@ If the digits can't be rearranged to form a bigger number, return -1 (or nil in 
 """
 
 def next_bigger(n):
-    pass
+    n = list(str(n))
+    temp = []
+    for i in range(len(n) - 2, -1, -1):
+        if n[i] < n[i + 1]:
+            n[i], n[i + 1] = n[i + 1], n[i]
+            if len(n[i + 1:]) > 0:
+                temp = n[i + 1:]
+                del n[i + 1:]
+                n.insert(-1,sorted(temp))
+                return n
+    return -1
+
+# Teste
+
+print(next_bigger(12))
