42650d089110826e0ebfb1a18ab01e0e50fc74eb
kennedh <ken.cmarcellino@gmail.com>
2026-03-05 20:00:00 -0300

    Update finalizado

--- The observed PIN.py
+++ The observed PIN.py
@@ -30,23 +30,25 @@ getPINs (get_pins in python, GetPINs in C#). But please note that all PINs, the 
 
 Detective, we are counting on you!
 """
+from itertools import product
 
-adjacentes = {
-    '1': ['1', '2', '4'],
-    '2': ['2', '1', '3', '5'],
-    '3': ['3', '2', '6'],
-    '4': ['4', '1', '5', '7'],
-    '5': ['5', '2', '4', '6', '8'],
-    '6': ['6', '3', '5', '9'],
-    '7': ['7', '4', '8'],
-    '8': ['8', '5', '7', '9', '0'],
-    '9': ['9', '6', '8'],
-    '0': ['0', '8']
-}
-
-def teste():
-    print(adjacentes["1"][1])
+def get_pins(observed):
+    adjacentes = {
+        '1': ['1', '2', '4'],
+        '2': ['2', '1', '3', '5'],
+        '3': ['3', '2', '6'],
+        '4': ['4', '1', '5', '7'],
+        '5': ['5', '2', '4', '6', '8'],
+        '6': ['6', '3', '5', '9'],
+        '7': ['7', '4', '8'],
+        '8': ['8', '5', '7', '9', '0'],
+        '9': ['9', '6', '8'],
+        '0': ['0', '8']
+    }
+    possibilidades = [adjacentes[digito] for digito in observed]
+    res = [a for a in product(*possibilidades)]
+    return ["".join(i) for i in res]
 
 # Teste
 
-teste()
+print(get_pins("13"))
