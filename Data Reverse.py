f065ad4b9ef4338ff6916e8c50df473df89c5835
kennedh <ken.cmarcellino@gmail.com>
2026-03-16 20:35:53 -0300

    Create Data Reverse

---
+++ Data Reverse.py
@@ -0,0 +1,33 @@
+"""
+A stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.
+"""
+
+def data_reverse(data):
+    temp = []
+    res  = []
+    for n in range(7,len(data) + 1,8):
+        temp.insert(0,data[n - 7:n + 1])
+    for r in temp:
+        res.extend(r)
+    return res
+
+# Teste
+
+print(data_reverse([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]))
+
+# resultado esperado [1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
