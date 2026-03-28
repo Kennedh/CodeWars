10cac6fef4ec8ee03aeb8013e7b5c86605de6131
Kennedh <ken.cmarcellino@gmail.com>
2026-03-28 11:57:06 -0300

    Add files via upload

---
+++ Multiply.py
@@ -0,0 +1,12 @@
+"""
+This code does not execute properly. Try to figure out why.
+"""
+
+def multiply(a, b):
+   return a * b
+
+# Teste
+
+print(multiply(1, 2))
+print(multiply(2, 3))
+print(multiply(3, 4))
