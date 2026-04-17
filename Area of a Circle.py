3312d5f60b0803cb2137601060ec7c947c448f73
kennedh <ken.cmarcellino@gmail.com>
2026-04-17 20:48:39 -0300

    Create Area of a Circle

---
+++ Area of a Circle.py
@@ -0,0 +1,27 @@
+"""
+Complete the function which will return the area of a circle with the given radius.
+
+Returned value is expected to be accurate up to tolerance of 0.01.
+
+If the radius is not positive, raise ValueError.
+
+Example:
+
+circle_area(43.2673)      # returns 5881.248  (± 0.01)
+circle_area(68)           # returns 14526.724 (± 0.01)
+circle_area(0)            # raises ValueError
+circle_area(-1)           # raises ValueError
+"""
+
+import math
+
+def circle_area(r):
+    if r <= 0:
+        raise ValueError("Radius must be positive")
+    area = math.pi * r * r
+    return round(area, 3)
+
+# Teste
+
+print(circle_area(43.2673))  # 5881.248
+print(circle_area(68))        # 14526.724
