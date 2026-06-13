a2e027d35f86247f288672dc6dcf9f73a718551e
kennedh <ken.cmarcellino@gmail.com>
2026-06-13 10:14:50 -0300

    Create Volume of a Cuboid

---
+++ Volume of a Cuboid.py
@@ -0,0 +1,14 @@
+"""
+Bob needs a fast way to calculate the volume of a rectangular cuboid with three values: the length, width and
+height of the cuboid.
+
+Write a function to help Bob with this calculation.
+"""
+
+def get_volume_of_cuboid(length, width, height):
+    return length * width * height
+
+# Teste
+
+print(get_volume_of_cuboid(1, 2, 2))
+print(get_volume_of_cuboid(6.3, 2, 5))
