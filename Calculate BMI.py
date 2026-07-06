83b47b545875d8fa61037a11599967a9143f5615
kennedh <ken.cmarcellino@gmail.com>
2026-07-06 18:35:43 -0300

    Create Calculate BMI

---
+++ Calculate BMI.py
@@ -0,0 +1,22 @@
+"""
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+"""
+
+def bmi(weight, height):
+    b = weight / height ** 2
+    return ["Underweight", "Normal", "Overweight", "Obese"][(b > 18.5) + (b > 25) + (b > 30)]
+
+# Teste
+
+print(bmi(50, 1.80))
+print(bmi(80, 1.80))
+print(bmi(90, 1.80))
+print(bmi(100, 1.80))
