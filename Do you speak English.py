3b90a1eacfe5aa272d2973259dcbb5da9d9c85e3
kennedh <ken.cmarcellino@gmail.com>
2026-07-08 19:31:35 -0300

    Create Do you speak English

---
+++ Do you speak English.py
@@ -0,0 +1,21 @@
+"""
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string
+contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+"""
+
+def sp_eng(sentence):
+    return "english" in sentence.lower()
+
+# Teste
+
+print(sp_eng("english"))
+print(sp_eng("egnlish"))
+print(sp_eng("engliish"))
+print(sp_eng("1234egn lis;h"))
+print(sp_eng("1234english ;k"))
